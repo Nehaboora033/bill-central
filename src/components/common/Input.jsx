56bae1b8673f06c9ca16cac593ac8feb177f50de
Neha <nehaboora49@gmail.com>
2025-06-26 23:17:57 +0530

    added outline-none

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -5,7 +5,7 @@ const Input = ({ placeholderText, buttonClass, buttonText, svgColor, Inputclass 
     return (
         <>
             <div className={`bg-white rounded-tr-[50px] rounded-br-[50px] rounded-bl-[50px] border border-[#CBCBCB] p-[9px] flex w-full max-w-[467px] ${Inputclass}`}>
-                <input type="email" placeholder={`${placeholderText}`} className={`w-full ml-[13px]  p-[9px] placeholder-text-[#7F8A8E] placeholder-font-normal placeholder-text-base outline-none border-none pr-[13px] `} />
+                <input type="email" placeholder={`${placeholderText}`} className={`w-full ml-[13px]  p-[9px] placeholder-text-[#7F8A8E] placeholder-font-normal placeholder-text-base outline-none  pr-[13px] `} />
                 <Button buttonClass={`${buttonClass}`} buttonText={`${buttonText}`} svgColor={`${svgColor}`} />
             </div>
 
